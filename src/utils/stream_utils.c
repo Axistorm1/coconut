4fbf7f182e84f2cef09fcb868a20f8208b642e09
Gaël Laguna <gael.laguna@epitech.eu>
2025-01-25 14:07:10 +0100

    :bug: Fix error reports not being read

--- src/utils/stream_utils.c
+++ src/utils/stream_utils.c
@@ -7,7 +7,7 @@ bool is_file_stream_null(FILE *file_stream, char *error_message)
 {
     if (file_stream == NULL) {
         write(1, error_message, strlen(error_message));
-        return false;
+        return true;
     }
-    return true;
+    return false;
 }
